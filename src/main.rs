be63c507078b7d7f302a20fb5f902b04c9a80d02
ben lamothe <bl@732.one>
2020-03-14 15:22:28 -0400

    add basic logger

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
+use simplelog::Config as LogConfig;
+use simplelog::LevelFilter as LogLevelFilter;
+use simplelog::SimpleLogger;
+
+fn init_logging() {
+    let conf = LogConfig::default();
+    let level = LogLevelFilter::Info;
+
+    SimpleLogger::init(level, conf).expect("failed to start logger");
+}
+
 fn main() {
+    init_logging();
+
+    log::info!("Power notifier starting");
 }
